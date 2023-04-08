c99ec97529f6f5e49bc7bcf0aad2c9d062c43b58
amh-ED <ahmed.kh6204@gmail.com>
2023-04-08 20:26:48 +0200

    modified after comments

--- main.cpp
+++ main.cpp
@@ -4,30 +4,37 @@
 using namespace sf;
 using namespace std;
 
-void PLAY_MUSIC_FROM_FILE(const std::string& filePath, sf::Music& music); //function to play music
+void playMusicFromFile(string file_path, Music& music); //function to play music
+
+const int window_width =1000;
+const int window_height =1000;
+const string window_name="git started";
 
 int main() {
 
-    sf::Music music;
+    Music music;
 
-    PLAY_MUSIC_FROM_FILE( "audio/lepo.wav", music);/* 1-path of the music is written as a spring 
-    2- mp3 require licence 3-i made it to accept the music as an argument to be able to view it inside the function  */
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    playMusicFromFile( "audio/lepo.wav", music);/* 1-path of the music is written as a string 
+    2- mp3 require licence (i tried to play mp3 files alot of time and it failed and found out that it require some soet of licence 
+    but you can change the file type using Audacity)
+    * 3-i made it to accept the music as an argument to be able to view it inside the function  */
+    
+    RenderWindow window( VideoMode(window_width, window_height), window_name);
 
     while (window.isOpen()) {
         sf::Event event;
 
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == Event::Closed) {
                 window.close();
             }
 
-            if (event.type == sf::Event::KeyPressed) {
+            if (event.type == Event::KeyPressed) {
                 if (event.key.code == Keyboard::Key::Space) {
                     if (music.getStatus() == sf::SoundSource::Status::Playing) {
                         music.pause(); //if music is being played --> pause it
                     }
-                    else if (music.getStatus() == sf::SoundSource::Status::Paused) {
+                    else if (music.getStatus() == SoundSource::Status::Paused) {
                         music.play(); //if music is paused --> play it
 
                     }
@@ -35,11 +42,11 @@ int main() {
                 if (event.key.code == Keyboard::Key::Escape) {
                     music.stop(); // way to close the music
                 }
-                 if (event.key.code == sf::Keyboard::Key::Up) {  //volume can be from(0 to 1) 0 --> muted 1 --> the oringinal volume
+                 if (event.key.code == Keyboard::Key::Up) {  //volume can be from(0 to 1) 0 --> muted 1 --> the oringinal volume
                     music.setVolume(music.getVolume() + 10); //new volume = old volume +10%
                 
                 }
-                if (event.key.code == sf::Keyboard::Key::Down) { //new volume = old volume -10%
+                if (event.key.code == Keyboard::Key::Down) { //new volume = old volume -10%
                     music.setVolume(music.getVolume() - 10);
                
                 }
@@ -52,9 +59,9 @@ int main() {
 
     return 0;
 }
-void PLAY_MUSIC_FROM_FILE(const std::string& filePath, sf::Music& music) {
+void playMusicFromFile(const string file_path, Music& music) {
 
-    if (!music.openFromFile(filePath)) {
+    if (!music.openFromFile(file_path)) {
         cout << "error";
     }
     music.setLoop(true);
