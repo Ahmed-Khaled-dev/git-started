a8c617d8478712d469d153bb1469c6223c2d680f
ruby <rehab.khaled.bahnasy@gmail.com>
2023-04-08 05:25:58 +0200

    added cursor

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 500), "GIT Started");
     std:: string user_input;
-      //user text
     sf :: Font font;
     font.loadFromFile("font/Roboto-Black.ttf");
     sf::Text text("", font);
-    sf :: Clock clock;
+    sf :: Clock clock; //for cursor
     while (window.isOpen())
     {
         sf::Event event;
@@ -18,21 +17,31 @@ int main()
             if (event.type == sf::Event::Closed){
                 window.close();
             }
-         else if (event.type == sf::Event::TextEntered) {
-                if (std::isprint(event.text.unicode))
+         else if (event.type == sf::Event::TextEntered) {  //take input from user
+                if (std::isprint(event.text.unicode))      //filter out symbols (only characters in ascii code enters)
                     user_input+= event.text.unicode;
             }
-            else if (event.type == sf::Event::KeyPressed) {
+            else if (event.type == sf::Event::KeyPressed) {    //if user wants to erase what he wrote
                 if (event.key.code == sf::Keyboard::BackSpace) {
                     if (!user_input.empty())
-                        user_input.pop_back();
+                        user_input.pop_back();             //pop it from the string
                 }
-                if (event.key.code == sf::Keyboard::Return) {
+                if (event.key.code == sf::Keyboard::Return) {  //user clicks enter (will be modified into appearing separately)
                         user_input += '\n';
                 }
             }
+        }static sf::Time text_effect_time; 
+        static bool show_cursor;
+
+        text_effect_time += clock.restart();
+
+        if (text_effect_time >= sf::seconds(0.5f)) //cursor time to appear
+        {
+            show_cursor = !show_cursor;
+            text_effect_time = sf::Time::Zero;
         }
-        text.setString(user_input);
+
+        text.setString(user_input + (show_cursor ? '|' : ' '));
         window.clear();
         window.draw(text);
         window.display();
