6ad5ae5e683d3a3c5ca1aaaa8cd123470aa09281
ruby <rehab.khaled.bahnasy@gmail.com>
2023-04-08 14:19:46 +0200

    Review modifications

--- main.cpp
+++ main.cpp
@@ -1,45 +1,49 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+using namespace sf;
+using namespace std;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 500), "GIT Started");
-    std:: string user_input,final_input;
-    sf :: Font font;
+    const int X=1000,Y=500;
+    const string title ="GIT Started" ;
+    RenderWindow window(VideoMode(X, Y) , title);
+    string user_input,final_input;
+    Font font;
     font.loadFromFile("font/Roboto-Black.ttf");
-    sf::Text text("", font);
-    sf::Text text_final("", font);
-    sf :: Clock clock; //for cursor
+    Text text("", font);
+    Text text_final("", font);
+    Clock clock; //for cursor
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed){
+            if (event.type == Event::Closed){
                 window.close();
             }
-         else if (event.type == sf::Event::TextEntered) {  //take input from user
-                if (std::isprint(event.text.unicode))      //filter out symbols (only characters in ascii code enters)
+         else if (event.type == Event::TextEntered) {  //take input from user
+                if (isprint(event.text.unicode))      //filter out symbols (only characters in ascii code enters)
                     user_input+= event.text.unicode;
             }
-            else if (event.type == sf::Event::KeyPressed) {    //if user wants to erase what he wrote
-                if (event.key.code == sf::Keyboard::BackSpace) {
+            else if (event.type == Event::KeyPressed) {    //if user wants to erase what he wrote
+                if (event.key.code == Keyboard::BackSpace) {
                     if (!user_input.empty())
                         user_input.pop_back();             //pop it from the string
                 }
-                if (event.key.code == sf::Keyboard::Return) {  //user clicks enter and the text will be transfered at the top of the screen
+                if (event.key.code == Keyboard::Return) {  //user clicks enter and the text will be transfered at the top of the screen
                         final_input += ( user_input + "\n") ;
                         user_input.clear();
                 }
             }
-        }static sf::Time text_effect_time; 
+        }static Time text_effect_time; 
         static bool show_cursor;
 
         text_effect_time += clock.restart();
 
-        if (text_effect_time >= sf::seconds(0.5f)) //cursor time to appear
+        if (text_effect_time >= seconds(0.5f)) //cursor time to appear
         {
             show_cursor = !show_cursor;
-            text_effect_time = sf::Time::Zero;
+            text_effect_time = Time::Zero;
         }
 
         text.setString(user_input + (show_cursor ? '|' : ' ')); //shape of cursor
