e9736a4dbd8269106c19fae78423ef76d678e70b
Haneen <haneenhany1205@gmail.com>
2023-04-11 00:44:38 +0200

    added sprite and font and moved game loop in main

--- main.cpp
+++ main.cpp
@@ -5,61 +5,73 @@
 using namespace std;
 using namespace sf;
 
-void drawDialogue(RenderWindow& window, string title, string imagePath, string font_type) {
+const int window_width = 1920;
+const int window_height = 1080;
+//structs
+struct declaration
+{
     Font font;
-    font.loadFromFile(font_type);
-
-    Texture texture;
-    texture.loadFromFile(imagePath);
-
-    RectangleShape dialogueBox(Vector2f(700, 200));
-    dialogueBox.setFillColor(Color::White);
-    dialogueBox.setOutlineThickness(5);
-    dialogueBox.setOutlineColor(Color::Black);
-    dialogueBox.setPosition((window.getSize().x - dialogueBox.getSize().x) / 2, window.getSize().y - dialogueBox.getSize().y);
-    //setPosition(150, 400)
-
+    Texture texture; 
     Sprite sprite;
-    sprite.setTexture(texture);
-    sprite.setScale(0.7, 0.7);
-    sprite.setPosition(50, 550);
-
     Text dialogueTitle;
-    dialogueTitle.setString(title);
-    dialogueTitle.setFont(font);
-    dialogueTitle.setFillColor(Color::Black); //...controls text color
-    dialogueTitle.setCharacterSize(35);    //...controls text size
-    dialogueTitle.setPosition(350, 610);
+}dialogue_box;
 
-    bool dialogue_open = true;
-    while (window.isOpen() && dialogue_open) {
+//functions
+void drawDialogue(RenderWindow& window, string title, string imagePath, string font_type, declaration& dialogue_box,RectangleShape& dialogueBox);
+
+
+int main()
+{
+    RenderWindow window(VideoMode(window_width, window_height), "SFML Dialogue Box");
+
+    string title = "Title";
+    string image_path = "man_photo\\man.png";
+    string font_type= "font_type\\Roboto-Black.ttf";
+    RectangleShape dialogueBox(Vector2f(750,300));
+
+    drawDialogue(window, title, image_path,font_type, dialogue_box,dialogueBox);
+    while (window.isOpen()) 
+    {
         Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
+        while (window.pollEvent(event)) 
+        {
+            if (event.type == Event::Closed)
+            {
                 window.close();
             }
-            else if (event.type == Event::KeyPressed && event.key.code == Keyboard::Enter) {
-                dialogue_open = false;
-            }
+            
         }
 
         window.clear(Color::White);
         window.draw(dialogueBox);
-        window.draw(dialogueTitle);
-        window.draw(sprite);
+        window.draw(dialogue_box.dialogueTitle);
+        window.draw(dialogue_box.sprite);
         window.display();
     }
+    return 0;
 }
 
-int main()
+void drawDialogue(RenderWindow& window, string title, string imagePath, string font_type, declaration& dialogue_box,RectangleShape& dialogueBox) 
 {
-    RenderWindow window(VideoMode(1000, 800), "SFML Dialog Box");
+    
+    dialogue_box.font.loadFromFile(font_type);
+    dialogue_box.texture.loadFromFile(imagePath);
 
-    string title = "Title";
-    string image_path = "C:\\Users\\haneenpc\\Desktop\\SP Project\\new\\git-started\\man\\man.png";
-    string font_type= "C:\\Users\\haneenpc\\Desktop\\SP Project\\new\\git-started\\font\\Roboto-Black.ttf";
+    dialogueBox.setFillColor(Color::White);
+    dialogueBox.setOutlineThickness(5);
+    dialogueBox.setOutlineColor(Color::Black);
+    dialogueBox.setPosition((window.getSize().x - dialogueBox.getSize().x) / 2, window.getSize().y - dialogueBox.getSize().y);
+    
 
-    drawDialogue(window, title, image_path,font_type);
+    
+    dialogue_box.sprite.setTexture(dialogue_box.texture);
+    dialogue_box.sprite.setScale(0.8, 0.8);
+    dialogue_box.sprite.setPosition(450, 750);
 
-    return 0;
+    
+    dialogue_box.dialogueTitle.setString(title);
+    dialogue_box.dialogueTitle.setFont(dialogue_box.font);
+    dialogue_box.dialogueTitle.setFillColor(Color::Black); //...controls text color
+    dialogue_box.dialogueTitle.setCharacterSize(38);    //...controls text size
+    dialogue_box.dialogueTitle.setPosition(700, 800);
 }
