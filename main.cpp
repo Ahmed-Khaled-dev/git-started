4bc2c15d344f62140587dab30f8383f198c8bd15
Rahma <Rahmakhattab07@gmail.com>
2023-04-10 21:33:30 +0200

    game loop moved into main

--- main.cpp
+++ main.cpp
@@ -1,45 +1,59 @@
-#include <bits/stdc++.h>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include<string>
 using namespace std;
 using namespace sf;
-void txt (RenderWindow& window)
+
+struct properties  //...with default values
 {
-    Clock clock;
-    Time time; //...=0
-    Font my_font;
-    my_font.loadFromFile("fonts/Roboto-Black.ttf");
-    Text my_text;
-    my_text.setFont(my_font);
-    my_text.setFillColor(Color::Blue); //...controls text color
-    my_text.setCharacterSize(35);    //...controls text size
-    my_text.setPosition(270,90);
-    String script = "This is our game\ngit-started\nwelcome boo!"; //...add script
-    while (window.isOpen())
-    {
-        Event e;
-        while (window.pollEvent(e))
-        {
-            if (e.type ==Event::Closed)
-                window.close();
-        }
-        time += clock.restart();
-        while (time >=seconds(0.08f))  //...change speed
-        {
-            time -=seconds(0.08f);  //...change speed
-            if (script.getSize() > 0)
-            {
-                my_text.setString(my_text.getString() + script[0]); //start from index zero
-                script = script.toAnsiString().substr(1);  //deletes index zero and re-indexes
-            }
-        }
-        window.clear(Color::White);
-        window.draw(my_text);
-        window.display();
-    } 
-}
+    String font_type = "fonts/Roboto-Black.ttf";
+    Color text_color = Color::Blue;
+    double text_size = 35;
+    double text_speed = 0.08f;
+    String dialogue_script = "This is our game\ngit-started\nwelcome boo!";
+} dialogue_properties;
+
+void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock& clock, Font& dialogue_text_font);
+
 int main()
 {
-    RenderWindow window({800, 300}, "dialogue-text-function");
-    txt(window);
+    RenderWindow window({ 1920, 1080 }, "dialogue-text-function"); 
+    Event e;
+    Clock clock;
+    Time dialogue_text_time; //...default value=0
+    Font dialogue_text_font;
+    Text dialogue_text_text;
+    while (window.isOpen())
+    {
+        while (window.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
+                window.close();
+        }
+        printDialogueText(dialogue_text_time, dialogue_text_text, clock,dialogue_text_font);
+        window.clear(Color::White);
+        window.draw(dialogue_text_text);
+        window.display();
+    }
     return 0;
+}
+
+void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock& clock, Font& dialogue_text_font)
+{
+        dialogue_text_time += clock.restart();
+        dialogue_text_font.loadFromFile(dialogue_properties.font_type);
+        dialogue_text_text.setFont(dialogue_text_font);
+        dialogue_text_text.setFillColor(dialogue_properties.text_color); //...controls text color
+        dialogue_text_text.setCharacterSize(dialogue_properties.text_size);    //...controls text size
+        dialogue_text_text.setPosition(820, 450);
+        while (dialogue_text_time >= seconds(dialogue_properties.text_speed))  //...change speed
+        {
+            dialogue_text_time -= seconds(dialogue_properties.text_speed);  //...change speed
+            dialogue_properties.dialogue_script;
+            if (dialogue_properties.dialogue_script.getSize() > 0)
+            {
+                dialogue_text_text.setString(dialogue_text_text.getString() + dialogue_properties.dialogue_script[0]); //start from index zero
+                dialogue_properties.dialogue_script = dialogue_properties.dialogue_script.toAnsiString().substr(1);  //deletes index zero and re-indexes
+            }
+        }   
 }
