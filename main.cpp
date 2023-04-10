eb098303aa8104145cecb2c80083e5a03a98d070
Akayiz <ahmed.khaled.yousry@gmail.com>
2023-04-10 06:51:47 +0200

    Added multiple commits functionality

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 struct commit{
     string message;
-    CircleShape circle;
+    Sprite sprite;
 };
 
-void addCommit(int &commits_count, commit commits[], string commit_message, int window_length, int window_width);
+void addCommit(int &commits_count, commit commits[], Texture& commit_textures, string commit_message, int window_length, int window_width);
 
-const int WINDOW_LENGTH = 1920, WINDOW_WIDTH = 1080;
+const int WINDOW_X = 1600, WINDOW_Y = 1000;
 const string GAME_TITLE = "Git Started";
 
-RenderWindow window(VideoMode(WINDOW_LENGTH, WINDOW_WIDTH), GAME_TITLE);
+RenderWindow window(VideoMode(WINDOW_X, WINDOW_Y), GAME_TITLE);
 
 int main(){
 
@@ -22,6 +22,13 @@ int main(){
     const int MAX_COMMITS = 100;
 
     commit commits[MAX_COMMITS];
+
+    Texture commit_textures;
+    if (!commit_textures.loadFromFile("resources/sprites/commits_sprites.png"))
+    {
+        cout << "Error loading commit sprites" << endl;
+    }
+    commit_textures.setSmooth(true);
 
     while (window.isOpen())
     {
@@ -34,13 +41,14 @@ int main(){
             }
             else if (Keyboard::isKeyPressed(Keyboard::Up))
             {
-                addCommit(commits_count, commits, "initial commit", WINDOW_LENGTH, WINDOW_WIDTH);
+                addCommit(commits_count, commits, commit_textures, "initial commit", WINDOW_X, WINDOW_Y);
             }
         }
-        window.clear(Color::Black);
+        window.clear({43, 45, 47});
         for (int i = 0; i < commits_count; i++)
         {
-            window.draw(commits[i].circle);
+            window.draw(commits[i].sprite);
+            //cout << "Commit number " << i << " X-position -> " << commits[i].sprite.getPosition().x << endl;
         }
         window.display();
     }
@@ -48,21 +56,32 @@ int main(){
     return 0;
 }
 
-void addCommit(int &commits_count, commit commits[], string commit_message, int window_length, int window_width){
+void addCommit(int& commits_count, commit commits[], Texture& commit_textures, string commit_message, int window_length, int window_width){
+    // All the following are the same for both conditions
+    Sprite commit_sprite;
+    commit_sprite.setTexture(commit_textures);
+    commit_sprite.scale(Vector2f(0.5, 0.5));
+    //commit_sprite.setColor({241, 80, 47});
     if (commits_count == 0)
     {
-        CircleShape circle(25.f);
-        circle.setFillColor(Color::Red);
-        circle.setPosition(window_length/2, window_width/3);
-        //circle.setPointCount(1000000);
-
-        commit new_commit = {commit_message, circle};
-        commits[commits_count] = new_commit;
-        commits_count++;
+        // I cut from the texture a circle **without** an arrow
+        commit_sprite.setTextureRect(IntRect(287, 70, 156, 156)); 
+        commit_sprite.setPosition(window_length/2, window_width/3);
     }
     else
     {
-        // Push the old commit $x to the left, then add a new commit in the middle of the screen
-        // Then add an arrow that points from the new commit to the old commit
+        const int circle_length = 60;
+        const int arrow_length = 125;
+        for (int i = 0; i < commits_count; i++)
+        {
+            commits[i].sprite.move(Vector2f(-(circle_length + arrow_length), 0));
+        }
+        // I cut from the texture a circle **with** an arrow
+        commit_sprite.setTextureRect(IntRect(37, 278, 406, 432));
+        commit_sprite.setPosition(window_length/2 - arrow_length, window_width/3);
     }
+
+    commit new_commit = {commit_message, commit_sprite};
+    commits[commits_count] = new_commit;
+    commits_count++;
 }
