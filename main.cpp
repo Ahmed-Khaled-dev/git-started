5e6a20820b912df0d99dff373f54378ff12f4fa2
Rahma <Rahmakhattab07@gmail.com>
2023-04-13 18:30:54 +0200

    updates according to comments

--- main.cpp
+++ main.cpp
@@ -8,43 +8,42 @@ using namespace sf;
 const int window_width = 1920;
 const int window_height = 1080;
 
-
 //structs
-struct declaration //for dialogue box
+struct dialogueBox_declration //for dialogue box
 {
     Font font;
     Texture texture; 
     Sprite sprite;
     Text dialogueTitle;
+    string title = "Title";
+    string image_path = "resources/sprites/man.png";
+    string font_type= "resources/fonts/Roboto-Black.ttf";
 }dialogue_box;
 
-struct properties  //for dialogue text
+struct dialogueText_declration  //for dialogue text
 {
-    String font_type = "fonts/Roboto-Black.ttf";
+    String font_type = "resources/fonts/Roboto-Black.ttf";
     Color text_color = Color::Blue;
     double text_size = 35;
-    double text_speed = 0.08f;
+    double text_speed = 0.09f;
     String dialogue_script = "This is our game\ngit-started\nwelcome boo!";
 } dialogue_properties;
 
-//functions
-void drawDialogue(RenderWindow& window,string title, string imagePath, string font_type, declaration& dialogue_box,RectangleShape& dialogueBox);
+//functions definition 
+void drawDialogue(RenderWindow& window, dialogueBox_declration& dialogue_box,RectangleShape& dialogueBox);
 void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock& clock, Font& dialogue_text_font);
 
 
 int main()
 {
     RenderWindow window(VideoMode(window_width, window_height), "Git Started!");
-    string title = "Title";
-    string image_path = "man_photo\\man.png";
-    string font_type= "font_type\\Roboto-Black.ttf";
     RectangleShape dialogueBox(Vector2f(750,300));
     Event event;
     Clock clock;
     Time dialogue_text_time; //...default value=0
     Font dialogue_text_font;
     Text dialogue_text_text;
-
+    
     //Game loop
     while (window.isOpen())
     {
@@ -53,7 +52,7 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
-        drawDialogue(window,title, image_path,font_type, dialogue_box,dialogueBox);
+        drawDialogue(window,dialogue_box,dialogueBox);
         printDialogueText(dialogue_text_time, dialogue_text_text, clock,dialogue_text_font);
         window.clear(Color::White);
         window.draw(dialogueBox);
@@ -62,20 +61,19 @@ int main()
         window.draw(dialogue_text_text);
         window.display();
     }
-    return 0;
 }
 
-void drawDialogue(RenderWindow& window, string title, string imagePath, string font_type, declaration& dialogue_box,RectangleShape& dialogueBox) 
+void drawDialogue(RenderWindow& window,dialogueBox_declration& dialogue_box,RectangleShape& dialogueBox) 
 {
     
-    dialogue_box.font.loadFromFile(font_type);
-    dialogue_box.texture.loadFromFile(imagePath);
+    dialogue_box.font.loadFromFile(dialogue_box.font_type);
+    dialogue_box.texture.loadFromFile(dialogue_box.image_path);
 
     //dialogue box
     dialogueBox.setFillColor(Color::White);
     dialogueBox.setOutlineThickness(5);
     dialogueBox.setOutlineColor(Color::Black);
-    dialogueBox.setPosition((window.getSize().x - dialogueBox.getSize().x) / 2, 800);
+    dialogueBox.setPosition((window.getSize().x - dialogueBox.getSize().x) / 2, window.getSize().y - dialogueBox.getSize().y);
     
     //sprite
     dialogue_box.sprite.setTexture(dialogue_box.texture);
@@ -83,11 +81,11 @@ void drawDialogue(RenderWindow& window, string title, string imagePath, string f
     dialogue_box.sprite.setPosition(450, 750);
 
     //title
-    dialogue_box.dialogueTitle.setString(title);
+    dialogue_box.dialogueTitle.setString(dialogue_box.title);
     dialogue_box.dialogueTitle.setFont(dialogue_box.font);
     dialogue_box.dialogueTitle.setFillColor(Color::Black); //...controls text color
     dialogue_box.dialogueTitle.setCharacterSize(38);    //...controls text size
-    dialogue_box.dialogueTitle.setPosition(700, 820);
+    dialogue_box.dialogueTitle.setPosition(700, 800);
 }
 
 void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock& clock, Font& dialogue_text_font)
@@ -97,7 +95,7 @@ void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock
         dialogue_text_text.setFont(dialogue_text_font);
         dialogue_text_text.setFillColor(dialogue_properties.text_color); //...controls text color
         dialogue_text_text.setCharacterSize(dialogue_properties.text_size);    //...controls text size
-        dialogue_text_text.setPosition(750, 900);
+        dialogue_text_text.setPosition(850, 850);
         while (dialogue_text_time >= seconds(dialogue_properties.text_speed))  //...change speed
         {
             dialogue_text_time -= seconds(dialogue_properties.text_speed);  //...change speed
