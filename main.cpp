a6566c293550e1c296e215811958ed68b2ee3a2c
Akayiz <ahmed.khaled.yousry@gmail.com>
2023-04-15 17:17:07 +0200

    Code refactoring

--- main.cpp
+++ main.cpp
@@ -5,44 +5,46 @@
 using namespace std;
 using namespace sf;
 
-const int window_width = 1920;
-const int window_height = 1080;
-
-//structs
-struct dialogueBox_declration //for dialogue box
+// Structs
+struct dialogueBox
 {
     Font font;
     Texture texture; 
     Sprite sprite;
-    Text dialogueTitle;
-    string title = "Title";
+    Text title;
+    string title_content = "Title";
+    double title_size = 38;
     string image_path = "resources/sprites/man.png";
     string font_type= "resources/fonts/Roboto-Black.ttf";
+    RectangleShape shape;
 }dialogue_box;
 
-struct dialogueText_declration  //for dialogue text
+struct dialogueText
 {
+    Font font;
+    Time time;
+    Clock clock;
+    Text text;
     String font_type = "resources/fonts/Roboto-Black.ttf";
-    Color text_color = Color::Blue;
-    double text_size = 35;
-    double text_speed = 0.09f;
-    String dialogue_script = "This is our game\ngit-started\nwelcome boo!";
-} dialogue_properties;
+    Color color = Color::Blue;
+    double size = 35;
+    double speed = 0.09f;
+    String script = "This is our game\ngit-started\nwelcome boo!";
+}dialogue_text;
 
-//functions definition 
-void drawDialogue(RenderWindow& window, dialogueBox_declration& dialogue_box,RectangleShape& dialogueBox);
-void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock& clock, Font& dialogue_text_font);
-
+// Functions definition 
+void drawDialogue(RenderWindow& window, dialogueBox& dialogue_box);
+void printDialogueText(dialogueText& dialogue_text);
 
 int main()
 {
-    RenderWindow window(VideoMode(window_width, window_height), "Git Started!");
-    RectangleShape dialogueBox(Vector2f(750,300));
+    const int WINDOW_WIDTH = 1920;
+    const int WINDOW_HEIGHT = 1080;
+    RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Git Started!");
+    dialogue_box.font.loadFromFile(dialogue_box.font_type);
+    dialogue_box.texture.loadFromFile(dialogue_box.image_path);
+    dialogue_text.font.loadFromFile(dialogue_text.font_type);
     Event event;
-    Clock clock;
-    Time dialogue_text_time; //...default value=0
-    Font dialogue_text_font;
-    Text dialogue_text_text;
     
     //Game loop
     while (window.isOpen())
@@ -52,58 +54,56 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
-        drawDialogue(window,dialogue_box,dialogueBox);
-        printDialogueText(dialogue_text_time, dialogue_text_text, clock,dialogue_text_font);
+        drawDialogue(window, dialogue_box);
+        printDialogueText(dialogue_text);
         window.clear(Color::White);
-        window.draw(dialogueBox);
-        window.draw(dialogue_box.dialogueTitle);
+        window.draw(dialogue_box.shape);
+        window.draw(dialogue_box.title);
         window.draw(dialogue_box.sprite);
-        window.draw(dialogue_text_text);
+        window.draw(dialogue_text.text);
         window.display();
     }
 }
 
-void drawDialogue(RenderWindow& window,dialogueBox_declration& dialogue_box,RectangleShape& dialogueBox) 
+void drawDialogue(RenderWindow& window, dialogueBox& dialogue_box) 
 {
+    //Dialogue box
+    dialogue_box.shape.setSize(Vector2f(750,300));
+    dialogue_box.shape.setFillColor(Color::White);
+    dialogue_box.shape.setOutlineThickness(5);
+    dialogue_box.shape.setOutlineColor(Color::Black);
+    dialogue_box.shape.setPosition((window.getSize().x - dialogue_box.shape.getSize().x) / 2, window.getSize().y - dialogue_box.shape.getSize().y);
     
-    dialogue_box.font.loadFromFile(dialogue_box.font_type);
-    dialogue_box.texture.loadFromFile(dialogue_box.image_path);
-
-    //dialogue box
-    dialogueBox.setFillColor(Color::White);
-    dialogueBox.setOutlineThickness(5);
-    dialogueBox.setOutlineColor(Color::Black);
-    dialogueBox.setPosition((window.getSize().x - dialogueBox.getSize().x) / 2, window.getSize().y - dialogueBox.getSize().y);
-    
-    //sprite
+    //Sprite
     dialogue_box.sprite.setTexture(dialogue_box.texture);
     dialogue_box.sprite.setScale(0.8, 0.8);
     dialogue_box.sprite.setPosition(450, 750);
 
-    //title
-    dialogue_box.dialogueTitle.setString(dialogue_box.title);
-    dialogue_box.dialogueTitle.setFont(dialogue_box.font);
-    dialogue_box.dialogueTitle.setFillColor(Color::Black); //...controls text color
-    dialogue_box.dialogueTitle.setCharacterSize(38);    //...controls text size
-    dialogue_box.dialogueTitle.setPosition(700, 800);
+    //Title
+    dialogue_box.title.setString(dialogue_box.title_content);
+    dialogue_box.title.setFont(dialogue_box.font);
+    dialogue_box.title.setFillColor(Color::Black);
+    dialogue_box.title.setCharacterSize(dialogue_box.title_size);
+    dialogue_box.title.setPosition(700, 800);
 }
 
-void printDialogueText(Time& dialogue_text_time, Text& dialogue_text_text, Clock& clock, Font& dialogue_text_font)
+void printDialogueText(dialogueText& dialogue_text)
 {
-        dialogue_text_time += clock.restart();
-        dialogue_text_font.loadFromFile(dialogue_properties.font_type);
-        dialogue_text_text.setFont(dialogue_text_font);
-        dialogue_text_text.setFillColor(dialogue_properties.text_color); //...controls text color
-        dialogue_text_text.setCharacterSize(dialogue_properties.text_size);    //...controls text size
-        dialogue_text_text.setPosition(850, 850);
-        while (dialogue_text_time >= seconds(dialogue_properties.text_speed))  //...change speed
+        dialogue_text.time += dialogue_text.clock.restart();
+        dialogue_text.text.setFont(dialogue_text.font);
+        dialogue_text.text.setFillColor(dialogue_text.color);
+        dialogue_text.text.setCharacterSize(dialogue_text.size);
+        dialogue_text.text.setPosition(850, 850);
+        while (dialogue_text.time >= seconds(dialogue_text.speed))
         {
-            dialogue_text_time -= seconds(dialogue_properties.text_speed);  //...change speed
-            dialogue_properties.dialogue_script;
-            if (dialogue_properties.dialogue_script.getSize() > 0)
+            dialogue_text.time -= seconds(dialogue_text.speed);
+            dialogue_text.script;
+            if (dialogue_text.script.getSize() > 0)
             {
-                dialogue_text_text.setString(dialogue_text_text.getString() + dialogue_properties.dialogue_script[0]); //start from index zero
-                dialogue_properties.dialogue_script = dialogue_properties.dialogue_script.toAnsiString().substr(1);  //deletes index zero and re-indexes
+                // Start from index zero
+                dialogue_text.text.setString(dialogue_text.text.getString() + dialogue_text.script[0]); 
+                // Deletes index zero and re-indexes
+                dialogue_text.script = dialogue_text.script.toAnsiString().substr(1);
             }
         }   
 }
